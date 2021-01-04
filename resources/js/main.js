03102f16ae6ff265a8b4a6599149b3b6a33dc127
0uma <vladgavrish16@gmail.com>
2021-01-04 20:23:15 +0200

    Made few assignments log to console, finished #4

--- resources/js/main.js
+++ resources/js/main.js
@@ -27,85 +27,114 @@ function min(a, b) {
 		return(b);
 }
 
-// Выборка
+function ucFirst(a) {
 
-let nomer = prompt("Введите номер задания ([номер раздела].[номер задания])", '');
-
-switch(nomer) {
-
-// Первый номер
-
-	case "1.1": {
-
-		let a = prompt("Введите любую цифру", '');
-
-		let n = parseInt(a);
-
-		switch(n) {
-			case 0:
-				alert("Ноль");
-				break;
-			case 1:
-				alert("Один");
-				break;
-			case 2:
-				alert("Два");
-				break;
-			case 3:
-				alert("Три");
-				break;
-			case 4:
-				alert("Четыре");
-				break;
-			case 5:
-				alert("Пять");
-				break;
-			case 6:
-				alert("Шесть");
-				break;
-			case 7:
-				alert("Семь");
-				break;
-			case 8:
-				alert("Восемь");
-				break;
-			case 9:
-				alert("Девять");
-				break;
-		}
-	}
-	break;
-
-// Второй номер
-
-	case "1.2": {
-
-		let a1 = prompt("Введите натуральное число a <= b", '');
-		let a = parseInt(a1); 
-
-		let b1 = prompt("Введите натуральное число b >= a", '');
-		let b = parseInt(b1);
-
-		alert("Сумма всех четных чисел в интервале [" + a + "," + b + "] = " + sumeven(a, b));
-
-	}
-	break;
-
-// Третий номер
-	
-	case "1.3": {
-		let a1 = prompt("Введите число a", '');
-		let a = parseInt(a1); 
-
-		let b1 = prompt("Введите число b", '');
-		let b = parseInt(b1);
-
-		let c1 = prompt("Введите число c", '');
-		let c = parseInt(c1);
-
-		alert("Самое большое число - " + max(max(a, b), c) + ", самое маленькое - " + min(min(a, b), c));
-
-	}
-	break;
+	let str = a.charAt(0).toUpperCase() + a.slice(1);
+	return(str);
 }
 
+// Выборка
+
+let n = 1;
+
+while(n != 0) {
+
+	let nomer = prompt("Введите номер задания ([номер раздела].[номер задания]), 0 - чтобы прекратить", '');
+	n = parseInt(nomer);
+
+	switch(nomer) {
+
+	// Первый номер
+
+		case "1.1": {
+
+			let a = prompt("Введите любую цифру", '');
+
+			let n = parseInt(a);
+
+			switch(n) {
+
+				case 0:
+					alert("Ноль");
+				break;
+
+				case 1:
+					alert("Один");
+				break;
+
+				case 2:
+					alert("Два");
+				break;
+
+				case 3:
+					alert("Три");
+				break;
+
+				case 4:
+					alert("Четыре");
+				break;
+
+				case 5:
+					alert("Пять");
+				break;
+
+				case 6:
+					alert("Шесть");
+				break;
+
+				case 7:
+					alert("Семь");
+				break;
+
+				case 8:
+					alert("Восемь");
+				break;
+
+				case 9:
+					alert("Девять");
+				break;
+			}
+		}
+		break;
+
+	// Второй номер
+
+		case "1.2": {
+
+			let a1 = prompt("Введите натуральное число a <= b", '');
+			let a = parseInt(a1); 
+
+			let b1 = prompt("Введите натуральное число b >= a", '');
+			let b = parseInt(b1);
+
+			console.log("Сумма всех четных чисел в интервале [" + a + "," + b + "] = " + sumeven(a, b));
+		}
+		break;
+
+	// Третий номер
+
+		case "1.3": {
+			let a1 = prompt("Введите число a", '');
+			let a = parseInt(a1); 
+
+			let b1 = prompt("Введите число b", '');
+			let b = parseInt(b1);
+
+			let c1 = prompt("Введите число c", '');
+			let c = parseInt(c1);
+
+			console.log("Самое большое число - " + max( max(a, b), c) + ", самое маленькое - " + min( min(a, b), c));
+		}
+		break;
+
+	// Четвертый номер
+
+		case "1.4": {
+
+			let a = prompt("Введите текст с маленькой буквы");
+			
+			alert(ucFirst(a));
+		}
+		break;
+	}
+}
