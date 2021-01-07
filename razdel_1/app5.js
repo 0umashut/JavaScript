f9fd31caa19433b6df301f8577d9b078f9924ef6
0uma <vladgavrish16@gmail.com>
2021-01-07 17:45:00 +0200

    finished #5, fixed folder name

---
+++ razdel_1/app5.js
@@ -0,0 +1,17 @@
+p = document.createElement('p');
+document.body.after(p);
+
+function extract() {
+
+	let ind = document.getElementById('curr').value;
+
+	if(Number.isInteger( parseInt( ind.charAt(0) ) ))
+		p.innerHTML = "Ошибка, введенный формат не соответствует требуемому"
+	else
+		p.innerHTML = "Сумма = " + extractCurrencyValue(ind);
+}
+
+function extractCurrencyValue(str) {
+
+	return( parseInt( str.slice(1) ) );
+}
