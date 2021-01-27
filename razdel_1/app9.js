a3225d9a4f57892f1ff55709fcfb66ce194a91e4
0uma <vladgavrish16@gmail.com>
2021-01-27 15:49:20 +0200

    fixed #1.9

--- razdel_1/app9.js
+++ razdel_1/app9.js
@@ -1,11 +1,13 @@
-function start() {
+const javaScriptConfirm = (name) => {
+	if (confirm(`Привет ${name}, знаешь ли ты JavaScript?`)) {
+		return 1;
+	}
+}
 
-	let name = prompt("Как тебя зовут?", "");
-
-	let conf = confirm(`Привет ${name}, знаешь ли ты JavaScript?`);
-
-	if (conf == true)
-		alert("Молодец")
-	else
-		alert("Можно начать изучения прямо сейчас");
+const launch = () => {
+	if (javaScriptConfirm(prompt("Как тебя зовут?", "")) === 1) {
+		alert("Молодец");
+	} else {
+		alert("Можешь начать изучение прямо сейчас");
+	}
 }
